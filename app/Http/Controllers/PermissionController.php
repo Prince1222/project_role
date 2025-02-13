259476d96be08664244dee32c713c382641940c3
Anik Maharjan <maharjanprincetag@gmail.com>
2025-02-13 15:32:48 +0545

    permissions

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -3,22 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
     //This method will show permission page
     public function index(){
-
+        return view('permissions.list');
     }
 
     //This method will show create  permission page
     public function create(){
-        
+        return view('permissions.create');
     }
     
         //This method will show create  permission in DB
-        public function store(){
-        
+        public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name'=> 'required|unique:permissions|min:3'
+        ]);
+        if($validator->passes()){
+            Permission::create(['name' => $request->name]);
+            return redirect()->route('permissions.index')->with('success','Permission added successfully.');
+
+        }
+        else{
+            return redirect()->route('permissions.create')->withInput()->withErrors($validator);
+
+        }
         }
 
             //This method will show edit  permission page
