37c5b8b4692f291e36207331609b68836c738efc
Anik Maharjan <maharjanprincetag@gmail.com>
2025-02-13 13:57:02 +0545

    first commit

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+    //This method will show permission page
+    public function index(){
+
+    }
+
+    //This method will show create  permission page
+    public function create(){
+        
+    }
+    
+        //This method will show create  permission in DB
+        public function store(){
+        
+        }
+
+            //This method will show edit  permission page
+    public function edit(){
+        
+    }
+
+    
+            //This method will  update  permission 
+            public function update(){
+        
+            }
+    
+         //This method will  delete  permission in DB
+         public function destroy(){
+        
+         }
+}
